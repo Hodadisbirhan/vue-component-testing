84984aef1468982781183f7549f35352eae1a874
hodadis <hodadisbirhan80@gmail.com>
2024-05-12 06:40:50 +0300

    creating the check box component

---
+++ src/components/h/chechbox.vue
@@ -0,0 +1,24 @@
+<script setup>
+const props = defineProps({
+  label: {
+    type: String,
+    default: ''
+  },
+  value: { type: [String, Number] },
+  name: {
+    type: String
+  },
+  id: {
+    type: [String]
+  }
+})
+
+const model = defineModel()
+</script>
+
+<template>
+  <div>
+    <input type="checkbox" :id="id" :name="name" v-model="model" />
+    <label :for="id" v-if="label">{{ label }}</label>
+  </div>
+</template>
